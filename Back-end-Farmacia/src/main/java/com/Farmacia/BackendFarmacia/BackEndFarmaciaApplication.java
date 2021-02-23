9021cd8301276f285d4f319371ce66652aa9f15e
Lucas-schimit <lucas_schimit@outlook.com>
2021-02-23 11:18:15 -0300

    Criação do pacote Spring initializr

---
+++ Back-end-Farmacia/src/main/java/com/Farmacia/BackendFarmacia/BackEndFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.Farmacia.BackendFarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndFarmaciaApplication.class, args);
+	}
+
+}
---
+++ Back-end-Farmacia/src/test/java/com/Farmacia/BackendFarmacia/BackEndFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Farmacia.BackendFarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
