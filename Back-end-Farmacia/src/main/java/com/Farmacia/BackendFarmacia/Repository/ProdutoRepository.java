9c831a65f8e9e0603b67918a5c288a38026fe57a
Lucas-schimit <lucas_schimit@outlook.com>
2021-02-24 11:40:20 -0300

    Atualização Rository

--- Back-end-Farmacia/src/main/java/com/Farmacia/BackendFarmacia/Repository/ProdutoRepository.java
+++ Back-end-Farmacia/src/main/java/com/Farmacia/BackendFarmacia/Repository/ProdutoRepository.java
@@ -3,11 +3,12 @@ package com.Farmacia.BackendFarmacia.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Farmacia.BackendFarmacia.model.Produto;
 
 
-
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
 	public List<Produto> findAllByDescricaoContainingIgnoreCase(String descricao);
